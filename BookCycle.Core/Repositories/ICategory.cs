ee744ef5402ed01300b8711e2c1db563bd12d7ce
Samet İrkören <sametirkoren@gmail.com>
2020-02-27 22:56:51 +0300

    Generic Interface Create

---
+++ BookCycle.Core/Repositories/ICategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BookCycle.Core.Models;
+
+namespace BookCycle.Core.Repositories
+{
+    public interface ICategory:IRepository<Category>
+    {
+        Task<Category> GetWithBooksByIdAsync(int categoryId);
+    }
+}
