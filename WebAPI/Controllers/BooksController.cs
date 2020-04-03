6df4b2329b07d2f6153d59f8c4bb5a8956eb5da5
Samet İrkören <sametirkoren@gmail.com>
2020-04-03 18:58:47 +0300

    Book API CRUD Operations

---
+++ WebAPI/Controllers/BooksController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private IBookService _bookService;
+
+        public BooksController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet("getall")]
+
+        public IActionResult GetList()
+        {
+            var result = _bookService.GetList();
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+;       }
+
+
+        [HttpGet("getlistbycategory")]
+
+        public IActionResult GetListByCategory(int categoryId)
+        {
+            var result = _bookService.GetListByCategory(categoryId);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+            
+        }
+
+        [HttpGet("getbyid")]
+
+        public IActionResult GetById(int bookId)
+        {
+            var result = _bookService.GetListByCategory(bookId);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+
+        }
+
+
+        [HttpPost("add")]
+
+        public IActionResult Add(Book book)
+        {
+            var result = _bookService.Add(book);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+
+        }
+
+
+        [HttpPost("update")]
+
+        public IActionResult Update(Book book)
+        {
+            var result = _bookService.Update(book);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+
+        }
+
+        [HttpPost("delete")]
+
+        public IActionResult Delete(Book book)
+        {
+            var result = _bookService.Delete(book);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+
+        }
+
+
+    }
+}
