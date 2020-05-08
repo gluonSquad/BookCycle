5a56e3fa41518f9f561ed8cac72e097c93488d46
Samet İrkören <sametirkoren@gmail.com>
2020-05-08 22:09:43 +0300

    Implementing the admin user and roles

--- WebUI/IdentityInitializer.cs
+++ WebUI/IdentityInitializer.cs
@@ -2,11 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Identity;
 
 namespace WebUI
 {
     public static class IdentityInitializer
     {
-        
+        public static async Task SeedData(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
+        {
+            var adminRole = await roleManager.FindByNameAsync("Admin");
+            if (adminRole == null)
+            {
+                await roleManager.CreateAsync(new AppRole {Name = "Admin"});
+            }
+
+            var memberRole = await roleManager.FindByNameAsync("Member");
+            if (memberRole == null)
+            {
+                await roleManager.CreateAsync(new AppRole {Name = "Member"});
+            }
+
+
+            var adminUser = await userManager.FindByNameAsync("Samet");
+            if (adminUser == null)
+            {
+                AppUser user = new AppUser
+                {
+                    FirstName = "Samet",
+                    LastName = "İrkören",
+                    UserName = "Samet",
+                    Email = "sametirkoren@gmail.com"
+                };
+                await userManager.CreateAsync(user,"1");
+                await userManager.AddToRoleAsync(user, "Admin");
+            }
+        }
     }
 }
--- WebUI/Startup.cs
+++ WebUI/Startup.cs
@@ -3,6 +3,7 @@ using DataAccess.Concrete.EntityFramework.Contexts;
 using Entities.Concrete;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -18,13 +19,20 @@ namespace WebUI
                 .AddRazorRuntimeCompilation();
           
             services.AddDbContext<BookCycleContext>();
-            services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<BookCycleContext>();
+            services.AddIdentity<AppUser, AppRole>(opt =>
+            {
+                opt.Password.RequireDigit = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequiredLength = 1;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequireNonAlphanumeric = false;
+            }).AddEntityFrameworkStores<BookCycleContext>();
             services.AddControllersWithViews();
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, UserManager<AppUser> userManager , RoleManager<AppRole> roleManager)
         {
             if (env.IsDevelopment())
             {
@@ -32,6 +40,7 @@ namespace WebUI
             }
 
             app.UseRouting();
+            IdentityInitializer.SeedData(userManager, roleManager).Wait();
             app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
